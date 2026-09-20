ff34565d3ec6bda2e01f20aaf43c41fe9020a123
ibrahimabushmud-gif <ibrahimabushmud@gmail.com>
2026-09-20 21:49:41 +0300

    Refactor imports and exports in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,49 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    deleteDoc, 
+    doc, 
+    updateDoc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApaTlq9Dg8VrHyu3q3gfKQAPGZnb26tAM",
-  authDomain: "my-store-db-332db.firebaseapp.com",
-  projectId: "my-store-db-332db",
-  storageBucket: "my-store-db-332db.firebasestorage.app",
-  messagingSenderId: "742544976621",
-  appId: "1:742544976621:web:5f07e5b782c67a614f7ae4"
+    apiKey: "AIzaSyApaTlq9Dg8VrHyu3q3gfKQAPGZnb26tAM",
+    authDomain: "my-store-db-332db.firebaseapp.com",
+    projectId: "my-store-db-332db",
+    storageBucket: "my-store-db-332db.firebasestorage.app",
+    messagingSenderId: "742544976621",
+    appId: "1:742544976621:web:5f07e5b782c67a614f7ae4"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, collection, addDoc, getDocs, deleteDoc, doc, updateDoc };
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged };
+export { 
+    db, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    deleteDoc, 
+    doc, 
+    updateDoc,
+    getDoc
+};
+export { 
+    auth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged 
+};
