fdeaf8bb6f22687bbf0d22c6294a0fd42c572a5a
ibrahimabushmud-gif <ibrahimabushmud@gmail.com>
2026-09-20 07:52:53 -0400

    Update Firebase configuration with real credentials

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "سنضع_هذا_الرقم_في_الخطوة_التالية",
-  authDomain: "سنضع_هذا_في_الخطوة_التالية",
-  projectId: "سنضع_هذا_في_الخطوة_التالية",
-  storageBucket: "سنضع_هذا_في_الخطوة_التالية",
-  messagingSenderId: "سنضع_هذا_في_الخطوة_التالية",
-  appId: "سنضع_هذا_في_الخطوة_التالية"
+  apiKey: "AIzaSyApaTlq9Dg8VrHyu3q3gfKQAPGZnb26tAM",
+  authDomain: "my-store-db-332db.firebaseapp.com",
+  projectId: "my-store-db-332db",
+  storageBucket: "my-store-db-332db.firebasestorage.app",
+  messagingSenderId: "742544976621",
+  appId: "1:742544976621:web:5f07e5b782c67a614f7ae4"
 };
 
 const app = initializeApp(firebaseConfig);
